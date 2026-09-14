224571a38bb4e1f0a5d0422c6a761f663abb41a6
Detotix <detotix@darukano.de>
2026-09-14 16:34:38 +0200

    auto disabling presence on error

--- back/main.py
+++ back/main.py
@@ -15,7 +15,7 @@ import other.detect_vr
 import other.system
 import other.monado_tasks as monado_tasks
 import pipe_sending
-#import tracemalloc 
+#import tracemalloc
 #tracemalloc.start()
 
 from shared import shared, pipe, change
@@ -28,19 +28,19 @@ import sys
 import systemkey
 import other.system
 
-
-print(f"[MAIN] folder of installation : {Path(__file__).parent.parent}")
 T_START=perf_counter()
 DATA_FOLDER=f"{os.path.expanduser('~')}/.local/share/monadolay"
 
+print(f"[MAIN] folder of installation : {Path(__file__).parent.parent}")
+print(f"[MAIN] data folder : {DATA_FOLDER}")
 
 
 class close:
     #state
     closed=False
 
-        
-    #this is so it ignores errors that may accure while exiting    
+
+    #this is so it ignores errors that may accure while exiting
     def _noerrorexit(exctype, value, traceback):
         pass
 
@@ -51,9 +51,9 @@ class close:
 
             #turns off exeptions so it can close like intended
             sys.excepthook=close._noerrorexit
-            
+
             #saves time played
-            shared.saved_data["time_spend"]+=perf_counter()-T_START 
+            shared.saved_data["time_spend"]+=perf_counter()-T_START
             with open(f"{DATA_FOLDER}/data.json", "w") as f:
                 f.write(json.dumps(shared.saved_data))
             #closes lovr
@@ -75,7 +75,7 @@ class close:
             print("[MAIN] closing")
             shared.closed=True
             close.closed=True
-            
+
 
 #atexit_register(close)
 signal(SIGQUIT, close.close)
@@ -99,15 +99,15 @@ def menu_click(local_monado_task):
     else: shared.systemkey_right[2]=False
     if shared.systemkey_right[2]:
         #toggle menu
-        if "menu" in change.up("render")["render"]: 
+        if "menu" in change.up("render")["render"]:
             shared.direct_data.render["render"].remove("menu")
             change.up("render", update=True)
             local_monado_task.send({"name": "overlay_input_on", "info": None})
-        else: 
+        else:
             shared.direct_data.render["render"].append("menu")
             change.up("render", update=True)
             local_monado_task.send({"name": "overlay_input_off", "info": None})
-        
+
         change.up("data", {"datachange": True})
     shared.systemkey_right[1]=shared.systemkey_right[0]
 #end of buttons
@@ -121,18 +121,17 @@ def main():
     if not os.path.exists(DATA_FOLDER) or not os.path.exists(f"{DATA_FOLDER}/data.json"):
         os.makedirs(DATA_FOLDER,exist_ok=True)
         with open(f"{DATA_FOLDER}/data.json", "w") as f:
-            f.write(json.dumps({"time_spend":0}))
+            f.write(json.dumps({}))
     with open(f"{DATA_FOLDER}/data.json", "r") as f:
-        shared.saved_data=json.load(f)
-
+        shared.saved_data={"discord_presence":True, "time_spend":0} | json.load(f)
 
     #creates named pipes if they dont exist
     if not os.path.exists("/tmp/monadolay_pipe_pl"): os.mkfifo("/tmp/monadolay_pipe_pl")
     if not os.path.exists("/tmp/monadolay_pipe_lp"): os.mkfifo("/tmp/monadolay_pipe_lp")
-     
+
     #pipe.lp_pipe=open("/tmp/monadolay_pipe_lp", "r")
     pipe_sending.pipe.pl_pipe=open("/tmp/monadolay_pipe_pl", "w")
-     
+
     #threads
     #server_thread=Thread(target=server.run, daemon=True)
     #server_thread.start()
@@ -140,11 +139,11 @@ def main():
     pipe_thread.start()
     systemkey_thread=Thread(target=systemkey.main, daemon=True)
     systemkey_thread.start()
-     
-    
+
+
     #gets current mute state
     change.up("data", {"show_mute": other.system.is_mic_muted()})
-     
+
     #named_pipe.send_lua("show_mute",{"something":[shared.data["show_mute"]]})
 
     #checking if monado-service is running
@@ -158,13 +157,13 @@ def main():
     #initially turn on the overlay input
 
     local_monado_task.send({"name": "overlay_input_on", "info": None})
-    
+
     #main loop
     while True:
         sleep(0.05)
         shared.t4+=1
-        #snapshot = tracemalloc.take_snapshot() 
-        #top_stats = snapshot.statistics("lineno") 
+        #snapshot = tracemalloc.take_snapshot()
+        #top_stats = snapshot.statistics("lineno")
         #for stat in top_stats[:20]: print(stat)
         #print("-----"*5)
         if shared.t4==4:
--- back/presence.py
+++ back/presence.py
@@ -3,24 +3,26 @@ from time import perf_counter
 import shared
 
 class current_presence:
+    enabled=True
     rpc=None
     changetime=0
     playing=False
 
 def playing_game(game_name):
-    current_presence.playing=True
-    try:
-        current_presence.changetime=perf_counter()+40
-        current_presence.rpc.update(
-            state=f"Playing {game_name}",
-            name="Monadolay",
-            large_image="monadolay-playing",
-            large_text="Monadolay - Overlay for Monado",
-        )
-    except:
-        pass
+    if current_presence.enabled:
+        current_presence.playing=True
+        try:
+            current_presence.changetime=perf_counter()+40
+            current_presence.rpc.update(
+                state=f"Playing {game_name}",
+                name="Monadolay",
+                large_image="monadolay-playing",
+                large_text="Monadolay - Overlay for Monado",
+            )
+        except:
+            pass
 def stop_playing_game():
-    if current_presence.changetime<perf_counter():
+    if current_presence.changetime<perf_counter() and current_presence.enabled:
         current_presence.playing=False
         try:
             current_presence.rpc.update(
@@ -32,13 +34,18 @@ def stop_playing_game():
         except:
             pass
 def stop_presence():
-    print("[PRESENCE] closing")
-    current_presence.rpc.clear()
-    current_presence.rpc.close()
+    if current_presence.enabled:
+        print("[PRESENCE] closing")
+        current_presence.rpc.clear()
+        current_presence.rpc.close()
 def discord_presence():
-
-    client_id = "1517909906345295974"
-    RPC = Presence(client_id)
-    RPC.connect() 
-    current_presence.rpc=RPC
-    stop_playing_game()
+    if current_presence.enabled:
+        try:
+            client_id = "1517909906345295974"
+            RPC = Presence(client_id)
+            RPC.connect()
+            current_presence.rpc=RPC
+            stop_playing_game()
+        except:
+            current_presence.enabled=False
+            print("[PRESENCE] Discord is not open (disabling the presence!)")
